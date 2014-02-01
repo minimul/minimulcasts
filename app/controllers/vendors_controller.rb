0ad371b7cf195cc6bb99dee768751b5c2c8145ad
Christian <zarski@gmail.com>
2014-02-01 15:54:47 -0500

    Create a vendor on QBO and add before_action to create a vendor service object

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,5 +1,6 @@
 class VendorsController < ApplicationController
   before_action :set_vendor, only: [:show, :edit, :update, :destroy]
+  before_action :set_qb_service, only: [:create, :edit, :update, :destroy]
 
   # GET /vendors
   # GET /vendors.json
@@ -25,6 +26,11 @@ class VendorsController < ApplicationController
   # POST /vendors.json
   def create
     @vendor = Vendor.new(vendor_params)
+
+    vendor = Quickbooks::Model::Vendor.new
+    vendor.given_name = vendor_params[:name]
+    vendor.email_address = vendor_params[:email_address]
+    @vendor_service.create(vendor)
 
     respond_to do |format|
       if @vendor.save
@@ -78,6 +84,14 @@ class VendorsController < ApplicationController
 
 
   private
+
+    def set_qb_service
+      oauth_client = OAuth::AccessToken.new($qb_oauth_consumer, session[:token], session[:secret])
+      @vendor_service = Quickbooks::Service::Vendor.new
+      @vendor_service.access_token = oauth_client
+      @vendor_service.company_id = session[:realm_id]
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_vendor
       @vendor = Vendor.find(params[:id])
@@ -85,6 +99,6 @@ class VendorsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def vendor_params
-      params.require(:vendor).permit(:name)
+      params.require(:vendor).permit(:name, :email_address)
     end
 end
