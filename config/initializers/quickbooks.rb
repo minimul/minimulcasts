9f52c72bfb988a428b6adc4e05317b72824f7155
Christian <zarski@gmail.com>
2014-11-15 08:37:03 -0500

    Enable sandbox mode and add in OAuth consumer key and secret from shell envs

--- config/initializers/quickbooks.rb
+++ config/initializers/quickbooks.rb
@@ -1,6 +1,6 @@
 
-QB_KEY = "<insert>"
-QB_SECRET = "<insert>"
+QB_KEY = ENV['MINIMULCASTS_CONSUMER_KEY']
+QB_SECRET = ENV['MINIMULCASTS_CONSUMER_SECRET']
 
 $qb_oauth_consumer = OAuth::Consumer.new(QB_KEY, QB_SECRET, {
     :site                 => "https://oauth.intuit.com",
@@ -8,3 +8,5 @@ $qb_oauth_consumer = OAuth::Consumer.new(QB_KEY, QB_SECRET, {
     :authorize_url        => "https://appcenter.intuit.com/Connect/Begin",
     :access_token_path    => "/oauth/v1/get_access_token"
 })
+
+Quickbooks.sandbox_mode = true
