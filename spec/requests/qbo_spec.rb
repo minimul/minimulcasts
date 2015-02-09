5dd8ca34148daaaa5aba31869b69b214824f8de1
Christian <zarski@gmail.com>
2015-02-09 10:11:29 -0500

    More spec work

--- spec/requests/qbo_spec.rb
+++ spec/requests/qbo_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'QBO requests' do
   it 'send_invoice_with_custom_fields' do
     account = create(:account)
-    base = Quickbooks::Base.new(account)
+    base = Quickbooks::Base.new(account, :invoice)
     invoice = base.qr_model(:invoice)
     custom_field = base.qr_model(:custom_field)
     custom_field.name = "LFI Invoice #"
@@ -11,7 +11,11 @@ describe 'QBO requests' do
     custom_field.string_value = "LFI12345678"
     invoice.custom_fields = [custom_field]
     puts invoice.to_xml
-    #VCR.use_cassette("qbo/invoice_with_custom_fields") do
-    #end
+    #invoice_service = base.create_service_for(:invoice)
+    #invoice_service = Quickbooks::Base.new(account, :invoice)
+    #puts invoice_service
+    VCR.use_cassette("qbo/invoice_with_custom_fields") do
+      base.service.create(invoice)
+    end
   end
 end
