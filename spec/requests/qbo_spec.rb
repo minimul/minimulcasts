95b77d4b0970b0aeb074180c1a3e8b67f7c7997a
Christian <zarski@gmail.com>
2015-02-09 09:41:31 -0500

    Basic invoice starter showing the XML

--- spec/requests/qbo_spec.rb
+++ spec/requests/qbo_spec.rb
@@ -1,7 +1,17 @@
+require 'rails_helper'
 
 describe 'QBO requests' do
-  def send_invoice_with_custom_fields
-    VCR.use_cassette("qbo/invoice_with_custom_fields") do
-    end
+  it 'send_invoice_with_custom_fields' do
+    account = create(:account)
+    base = Quickbooks::Base.new(account)
+    invoice = base.qr_model(:invoice)
+    custom_field = base.qr_model(:custom_field)
+    custom_field.name = "LFI Invoice #"
+    custom_field.type = "StringType"
+    custom_field.string_value = "LFI12345678"
+    invoice.custom_fields = [custom_field]
+    puts invoice.to_xml
+    #VCR.use_cassette("qbo/invoice_with_custom_fields") do
+    #end
   end
 end
